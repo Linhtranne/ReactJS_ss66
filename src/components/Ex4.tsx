13e6cb66dee26f6a4e945af78e8b48aece050e73
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-06-18 13:04:51 +0700

    Hehehe

--- src/components/Ex4.tsx
+++ src/components/Ex4.tsx
@@ -22,14 +22,13 @@ const decrease = (payload: number) => ({
   payload,
 });
 
-export default function Bt4() {
+export default function Ex4() {
   const [count, setCount] = useState(store.getState());
   store.subscribe(() => {
     setCount(store.getState());
   });
   return (
     <div>
-      <h1>Bt4</h1>
       <button onClick={() => store.dispatch(increase(1))}>increase</button>
       <button onClick={() => store.dispatch(decrease(1))}>decrease</button>
       <p>{count}</p>
